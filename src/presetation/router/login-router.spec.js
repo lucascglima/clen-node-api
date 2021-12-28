4a0370cf913f2d7280a6232303be5be5d47124f4
Lucas Lima <lucascglima@gmail.com>
2021-12-28 18:36:39 -0300

    ensure LoginRouter returns 500 if no body or no httpRequest

--- src/presetation/router/login-router.spec.js
+++ src/presetation/router/login-router.spec.js
@@ -1,6 +1,12 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
@@ -9,7 +15,7 @@ class LoginRouter {
 }
 
 describe('Login Router', () => {
-  test('Shoud return 400 if no email provided', () => {
+  test('Shoud return 400 if no email is provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
@@ -19,7 +25,8 @@ describe('Login Router', () => {
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
-  test('Shoud return 400 if no password provided', () => {
+
+  test('Shoud return 400 if no password is provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
@@ -28,5 +35,17 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Shoud return 500 if no httpRequest is provided', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Shoud return 500 if htttpRequest has no body', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route({})
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
